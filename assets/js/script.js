d3df3305578b3c274ee40908e5f0334c7a466787
Max <maxrrice@hotmail.com>
2023-06-22 18:27:52 -0500

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@
 // select dom elements
   var startButton = document.querySelector('.start-button')
   var resetButton = document.querySelector('.reset-button')
-  var timerEl = document.querySelector('.timer')
+  var timerEl = document.querySelector('.timer-count')
   var wordBlanksEl = document.querySelector('.word-blanks')
   var winsEl = document.querySelector('.win')
   var lossesEl = document.querySelector('.lose')
-
+  var wins = 0
+  var losses = 0
+  var time = 10
+  var intervalId
 
   var words = ["jellyfish", "pancackes", "callback", "bacon", "porcupine"]
   var userGuesses = [] // list of letters guessed by user
@@ -40,11 +43,39 @@
 
   }
 
+  function tick() {
+    console.log("Tick...")
+    // check if userGuesses has all letters that are in answer string
+    // if has guess all letters
+    if (!wordBlanksEl.innerText.includes("_")) {
+      // end round
+      // increment wins
+      wins++
+      // display "you won!"
+      wordBlanksEl.innerText = "You Won!"
+
+    } else {
+      // is our timer count at 0?
+      if (time <= 0) {
+        clearInterval(intervalId)
+        timerEl.innerText = "Out of time!"
+        // diplay "lost round"
+        // increment losses
+
+      } else {
+        // decrement timer count
+        time--
+        // display count on page
+        timerEl.innerText = time
+
+      }
+    }
+  }
+
   function startRound() {
-    // TODO: start timer
     getRandomWord()
     renderWord()
-
+    intervalId = setInterval(tick, 1000)
   }
 
   function onKeyDown(event) {
@@ -58,22 +89,6 @@
     console.log(answer)
     console.log(event.key)
     console.log(userGuesses)
-  }
-
-  function tick() {
-    //TODO:
-    // check if userGuesses has all letters that are in answer string
-      // if has guess all letters
-        // end round
-        // increment wins
-        // display "you won!"
-      // else
-        // is our timer count at 0?
-          // if yes
-            // diplay "lost round"
-            // increment losses
-          // else
-            // decrement timer count
   }
 
   // Event Listeners
